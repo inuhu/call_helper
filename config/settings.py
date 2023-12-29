98782f7dd5fbedc2786fce296ee3b042aefb4369
Maksim Yarotskiy <iartoskiimaksim@gmail.com>
2023-12-30 02:08:59 +0300

    settings work

--- config/settings.py
+++ config/settings.py
@@ -32,6 +32,12 @@ INSTALLED_APPS += [
     'corsheaders',
 ]
 
+# apps
+INSTALLED_APPS += [
+    'api',
+    'common',
+]
+
 
 
 MIDDLEWARE = [
@@ -105,11 +111,8 @@ AUTH_PASSWORD_VALIDATORS = [
 
 
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_TZ = True
 
 
@@ -119,7 +122,6 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
-MEDIA_TEST_ROOT = os.path.join(BASE_DIR, 'media/test/')
 #=================
 
 
